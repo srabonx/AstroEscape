37459a1c493c49620c6719d1d7b81a53a3ea4a89
srabonx <srabon9012@gmail.com>
2024-08-18 23:18:18 +0600

    Follow cam + jump + ladder animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,23 +10,54 @@ public class PlayerMovement : MonoBehaviour
 
     Rigidbody2D m_rigidBody;
 
+    BoxCollider2D m_boxCollider;
+
     Animator m_animator;
 
     bool IsRunning;
+    bool IsJumping;
+    bool IsFalling;
+
+    bool IsClimbing;
+
+    bool IsOnGround;
 
     [SerializeField] float MoveSpeed = 10.0f;
-    [SerializeField] float JumpForce = 800.0f;
+    [SerializeField] float JumpForce = 1000.0f;
+    float   GravityScale = 4.0f;
+
+    int m_groundMask = 0;
 
 
     void Start()
     {
         m_rigidBody = GetComponent<Rigidbody2D>();
+        m_boxCollider = GetComponent<BoxCollider2D>();
         m_animator = GetComponent<Animator>();
+
+        GravityScale = m_rigidBody.gravityScale;
+
+        m_groundMask = LayerMask.GetMask("Ground");
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {       
+        if(m_boxCollider.IsTouchingLayers(m_groundMask))
+        {
+            IsOnGround = true;
+            IsFalling = false;
+            IsJumping = false;
+        }
+        else
+        {
+            IsOnGround = false;
+        }   
     }
 
     void Update()
     {
         Run();
+        ClimbLadder();
         FlipSprite();
         ChangeAnimation();
     }
@@ -40,7 +71,13 @@ public class PlayerMovement : MonoBehaviour
     {
         if(inputValue.isPressed)
         {
-            m_rigidBody.AddForce(new(0.0f,JumpForce));
+            if(IsOnGround)
+            {
+                m_rigidBody.AddForce(new(0.0f,JumpForce));
+                IsJumping = true;
+                IsFalling = false;
+                IsOnGround = false;
+            }
         }
     }
 
@@ -48,6 +85,13 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 playerVelocity = new(m_moveInput.x * MoveSpeed, m_rigidBody.velocity.y);
         m_rigidBody.velocity = playerVelocity;
+
+        if(m_rigidBody.velocity.y < 0 && !IsOnGround)
+        {
+            IsFalling = true;
+            IsJumping = false;        
+        }
+
     }
 
     void FlipSprite()
@@ -66,5 +110,38 @@ public class PlayerMovement : MonoBehaviour
     void ChangeAnimation()
     {
         m_animator.SetBool("IsRunning", IsRunning);
+
+        m_animator.SetBool("IsJumping", IsJumping);
+
+        m_animator.SetBool("IsFalling", IsFalling);
+
+        m_animator.SetBool("IsClimbing", IsClimbing);
+    }
+
+    void ClimbLadder()
+    {
+        if(!m_boxCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            m_rigidBody.gravityScale = GravityScale;
+            return;
+        }
+
+        IsJumping = false;
+        IsFalling = false;
+        IsOnGround = false;
+
+        Vector2 climbVelocity = new(m_rigidBody.velocity.x, m_moveInput.y * MoveSpeed);
+        m_rigidBody.velocity = climbVelocity;
+
+        if(m_moveInput.y != 0)
+        {
+            IsClimbing = true;
+        }
+        else
+        {
+            IsClimbing = false;
+        }
+
+        m_rigidBody.gravityScale = 0;
     }
 }
